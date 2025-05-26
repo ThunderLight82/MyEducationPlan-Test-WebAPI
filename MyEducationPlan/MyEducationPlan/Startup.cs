d495cdc95f29ffdfe0a26a6dff47639918219df0
Pecherky Oleg <yogidanfuzo@gmail.com>
2025-05-26 16:40:02 +0300

    Created [Startup.cs] to hold core startup logic. Separation from [Program.cs] lead to more coherent reading in future
    Changed [appsettings.json] to hold new SQL connection string from localhost and personal testing.

---
+++ MyEducationPlan/MyEducationPlan/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MyEducationPlan.DataAccess;
+
+namespace MyEducationPlan;
+
+public class Startup
+{
+    private IConfiguration Configuration { get; }
+    
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContextPool<EducationPlanDbContext>(options => options
+            .UseSqlServer(Configuration.GetConnectionString("SQLServer")));
+
+        RegisterServices(services);
+
+        services.AddMvc().AddNewtonsoftJson(options => 
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+        
+        services.AddEndpointsApiExplorer();
+        
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyEducationPlan", Version = "v1" });
+        });
+    }
+    
+    private void RegisterServices(IServiceCollection services)
+    {
+        // Future Service registration TODO
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+        
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        
+        // Future Endpoints for HomePage TODO
+        app.UseEndpoints(endpoints =>
+        {
+            
+        });
+        
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "UniversityManagement.WebApi");
+        });
+    }
+}
