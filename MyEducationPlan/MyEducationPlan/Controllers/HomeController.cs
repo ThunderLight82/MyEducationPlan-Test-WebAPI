a5d368de2a2cbca42ddbccec10f49da1585ffd22
Pecherky Oleg <yogidanfuzo@gmail.com>
2025-06-04 16:39:35 +0300

    Refactored and expanded [HomeController.cs] with new core logic

--- MyEducationPlan/MyEducationPlan/Controllers/HomeController.cs
+++ MyEducationPlan/MyEducationPlan/Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.AspNetCore.Mvc;
 using MyEducationPlan.Application.Services.Interfaces;
 
@@ -19,7 +18,7 @@ public class HomeController : Controller
     {
         if (!string.IsNullOrEmpty(errorMessage))
         {
-            ViewBag.ErrorMessage = errorMessage;
+            TempData["ErrorMessage"] = $"An error occurred: {errorMessage}";
         }
         
         var projects = await _projectService.GetProjects();
@@ -39,9 +38,64 @@ public class HomeController : Controller
         }
         catch (Exception ex)
         {
-            ViewBag.ErrorMessage = $"An error occurred: {ex.Message}";
+            TempData["ErrorMessage"] = $"An error occurred: {ex.Message}";
             
             return RedirectToAction("GetProjects", "Home", new { errorMessage = ViewBag.ErrorMessage });
         }
+    }
+    
+    //POST [CALCULATE AVERAGE RATING WITHIN SELECTED PROJECT]
+    [HttpPost]
+    public async Task<IActionResult> CalculateAverageRating(int projectId)
+    {
+        try
+        {
+            var average = await _projectService.CalculateAverageProjectRating(projectId);
+            TempData["AverageRating"] = average.HasValue 
+                ? $"Average Rating: {average:F2}" 
+                : "No feedbacks available in project to calculate average.";
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = $"An error occurred: {ex.Message}";
+        }
+        
+        return RedirectToAction("GetFeedbacksListByProjectId", new { projectId });
+    }
+    
+    //POST [DELETE ALL NEGATIVE FEEDBACKS IN SELECTED PROJECT]
+    [HttpPost]
+    public async Task<IActionResult> DeleteAllNegativeFeedbacks(int projectId)
+    {
+        try
+        {
+            await _projectService.DeleteAllNegativeFeedbacks(projectId);
+            
+            TempData["SuccessMessage"] = "All negative feedbacks deleted.";
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = $"An error occurred: {ex.Message}";
+        }
+
+        return RedirectToAction("GetFeedbacksListByProjectId", new { projectId });
+    }
+    
+    //POST [DELETE SELECTED NEGATIVE FEEDBACK IN PROJECT]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteSingleSelectedFeedback(int feedbackId, int projectId)
+    {
+        try
+        {
+            await _projectService.DeleteSingleFeedbackById(feedbackId);
+            TempData["SuccessMessage"] = "Feedback deleted successfully.";
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = $"An error occurred: {ex.Message}";
+        }
+
+        return RedirectToAction("GetFeedbacksListByProjectId", new { projectId });
     }
 }
